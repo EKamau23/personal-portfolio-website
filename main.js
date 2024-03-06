46fdc52de5ccea9d08547917ce3571c7156e9826
EKamau23 <mceliudk@gmail.com>
2024-03-06 12:38:15 +0300

    final commit

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+//two set of elements
+// switching between different content sections
 const tabLinks = document.getElementsByClassName("tab-links");
+// making the content sections corresponding to each tab
 const tabContents = document.getElementsByClassName("tab-contents");
 
 function openTab(tabName){
+    //removes active class ensure only selected tab is active
     for (tabLink of tabLinks){
         tabLink.classList.remove("active-link")
     }
     for (tabContent of tabContents){
         tabContent.classList.remove("active-tab")
     }
-    
+    //then adds active class to clicked tab link to highlight it
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 };
 
+//sidemenu containing navigation links once in a small screen
 const sideMenu = document.getElementById("sidemenu");
 
 function openMenu(){
